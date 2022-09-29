f2a7ac01bbe3d63a6f081929f25b306f52a48745
Matthias Urlichs <matthias@urlichs.de>
2022-09-29 16:16:06 +0200

    Initial check-in

---
+++ contextlog/_main.py
@@ -0,0 +1,97 @@
+import logging
+import threading
+
+from contextlib import contextmanager
+from contextvars import ContextVar
+
+context = ContextVar("context")
+
+_main_context = threading.local()
+
+logger = logging.getLogger("contextlog")
+
+class _Context:
+    def __init__(self, parent, msg, *args, level=logging.DEBUG):
+        self.parent = parent
+        self.children = set()
+        self.msg = msg
+        self.args = args
+        self.level = level
+        self._ctx = None
+
+    def __enter__(self):
+        if self._ctx is not None:
+            raise RuntimeError("Context %r entered twice")
+        if self.parent is not None:
+            self.parent.children.add(self)
+        self._ctx = context.set(self)
+
+        return self
+
+    def __exit__(self, *tb):
+        context.reset(self._ctx)
+        if self.parent is not None:
+            self.parent.children.remove(self)
+        self._ctx = None
+
+    def _log(self, indent=0, level=logging.DEBUG):
+        if callable(self.msg):
+            msg,args = msg(*self.args)
+        else:
+            msg,args = self.msg,self.args
+        logger.log(level, " "*indent + msg, *args)
+
+    def log_tree(self, indent=0, level=logging.DEBUG):
+        if self.want_log():
+            self._log(indent=indent, level=level)
+            for c in self.children:
+                c.log_tree(indent=indent+1, level=level)
+
+    def want_log(self, level=logging.DEBUG):
+        if self.level >= level:
+            return True
+        for c in self.children:
+            if c.want_log(level):
+                return True
+        return False
+
+    def log_backtrace(self, level=logging.DEBUG):
+        ctx = self
+        indent=0
+        while ctx is not None:
+            ctx._log(level=level, indent=indent)
+            indent += 1
+            ctx = ctx.parent
+
+@contextmanager
+def context_of(name, *args, level=logging.DEBUG):
+    with _Context(context.get(), name, *args, level=level) as ctx:
+        yield ctx
+
+@contextmanager
+def main_context(name="MAIN", *args):
+    with _Context(None, name, *args) as ctx:
+        if hasattr(_main_context, "main"):
+            raise RuntimeError("Parallel 'main_context' calls detected.")
+
+        try:
+            _main_context.main = ctx
+            yield ctx
+        finally:
+            del _main_context.main
+
+
+def log_backtrace(level=logging.DEBUG):
+    """
+    Log a back-trace of the current context
+    """
+    context.get().log_backtrace(level=level)
+
+
+def log_tree(level=logging.DEBUG):
+    _main_context.main.log_tree(level=level)
+
+
+def current_context():
+    return context.get()
+
---
+++ example/demo.py
@@ -0,0 +1,21 @@
+import anyio
+import logging
+from contextlog import main_context, context_of, log_tree
+
+async def delay(x):
+    with context_of("delay %.2f", x):
+        await anyio.sleep(x)
+
+async def test():
+    async with anyio.create_task_group() as tg:
+        with context_of("test"):
+            tg.start_soon(delay,0.1)
+            tg.start_soon(delay,0.2)
+            tg.start_soon(delay,0.25)
+            await anyio.sleep(0.15)
+            log_tree()
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    with main_context():
+        anyio.run(test)
